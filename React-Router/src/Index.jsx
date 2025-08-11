a0bf48938b8828dc593b1070ebfc5cb634207311
Umwizer <ruthumwizerwa@gmail.com>
2025-08-11 10:24:47 +0200

    importing routes

--- React-Router/src/Index.jsx
+++ React-Router/src/Index.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return <h1>Hello World</h1>;
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
