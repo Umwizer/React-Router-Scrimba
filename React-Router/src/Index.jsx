0e7808755688784b2998bc0f4ae9b74db27fbda3
Umwizer <ruthumwizerwa@gmail.com>
2025-08-18 12:28:07 +0200

    header and layout

--- React-Router/src/Index.jsx
+++ React-Router/src/Index.jsx
@@ -6,26 +6,19 @@ import About from "./pages/About";
 import "./Server.js";
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx";
+import Layout from "./components/Layout";
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
