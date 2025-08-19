a8342d3a34205866a286920a17d52c8fb88651b7
Umwizer <ruthumwizerwa@gmail.com>
2025-08-19 09:38:50 +0200

    react router

--- React-Router/src/Index.jsx
+++ React-Router/src/Index.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import "./Server.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import "./Server"; // starts MirageJS/mock server
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx";
-import Layout from "./components/Layout";
+import Layout from "./components/Layout.jsx";
+import Dashboard from "./pages/Host/Dashboard.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Review from "./pages/Host/Review.jsx";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/About" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Review />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
---
+++ React-Router/src/pages/Host/Review.jsx
@@ -0,0 +1,3 @@
+export default function Review() {
+  return <h1>Review goes here</h1>;
+}
