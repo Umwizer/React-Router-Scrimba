55c7d8ea1f4bda51a7d20971fdf29181a96cdfca
Umwizer <ruthumwizerwa@gmail.com>
2025-08-20 20:07:15 +0200

    creating hostvansdetails

--- React-Router/src/pages/Host/Income.jsx
+++ React-Router/src/pages/Host/Income.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
 export default function Income() {
-  return <h1>Income goes here</h1>;
+  return (
+    <>
+      <h1>Income goes here</h1>
+      <Link to="">Click Me</Link>
+    </>
+  );
 }
