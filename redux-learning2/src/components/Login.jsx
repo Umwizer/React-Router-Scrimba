dda700cb025bf972eea6ad1bdbfae244eef9414e
Umwizer <ruthumwizerwa@gmail.com>
2025-08-27 11:31:13 +0200

    redux

--- redux-learning2/src/components/Login.jsx
+++ redux-learning2/src/components/Login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Email:</p>
+    </div>
+  );
 };
 
 export default Login;
