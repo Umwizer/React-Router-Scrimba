59bc58fcadd08b938268d194e4660e096251a985
Umwizer <ruthumwizerwa@gmail.com>
2025-09-03 11:51:17 +0200

    red

---
+++ check/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState, type JSX } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App(): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  return <div></div>;
+}
+
+export default App;
---
+++ mental-questions/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+
+function DashBoard() {
+  return <h1>DashBoard</h1>;
+}
+function Home() {
+  return <h1>Home</h1>;
+}
+function About() {
+  return <h1>About</h1>;
+}
+function App() {
+  const activeStyle = {
+    fontWeight: "bold",
+    color: "blue",
+  };
+
+  return (
+    <div>
+      <Router>
+        <nav>
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/dashBoard"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            DashBoard
+          </NavLink>
+          <NavLink
+            to="/about"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            About
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/dashboard" element={DashBoard} />
+          <Route path="/about" element={About} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
